7c9d6e7d6e4261899338fd523b2e5cca8fc6d4dc
BuildTools <unconfigured@null.spigotmc.org>
2020-11-27 15:12:57 +0100

    Added logic for adding products
    Images don't work currently

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def user_exists(username, check_admin=False):
             query = "SELECT username FROM User WHERE username=%s;"
         cursor = conn.cursor()
         cursor.execute(query, (username,))
-        attr = cursor.fetchone() 
+        attr = cursor.fetchone()
         cursor.close()
         # attr is 'None' if no user was found
         if attr:
@@ -34,14 +34,14 @@ def add_user_db(username, password):
         conn.autocommit = False
         cursor = conn.cursor()
         lookup_query = 'SELECT username FROM User where username=%s'
-        cursor.execute(lookup_query, (username,)) 
+        cursor.execute(lookup_query, (username,))
 
         if cursor.fetchone():
             conn.rollback() # Unsure about this rollback
             cursor.close()
             return False # User already exists.
 
-        insert_query = 'INSERT INTO User(username, password) VALUES(%s, %s)'       
+        insert_query = 'INSERT INTO User(username, password) VALUES(%s, %s)'
         cursor.execute(insert_query, (username, password))
         conn.commit()
         cursor.close()
@@ -71,12 +71,22 @@ def log_in(username, password, as_admin=False):
             lookup_query = 'SELECT username FROM Admin where username=%s and password=%s'
         else:
             lookup_query = 'SELECT username FROM User where username=%s and password=%s'
-        cursor.execute(lookup_query, (username, password)) 
+        cursor.execute(lookup_query, (username, password))
 
         if cursor.fetchone():
             return True
     return False
-    
+
+def addProduct(pid, stock, description, price, brand):
+    print("AAAAAH")
+    with Conn_db() as conn:
+        print("lmao")
+        cursor = conn.cursor()
+        query = "INSERT INTO product(idproduct, price, stock, description, brand) VALUES(%s,%s,%s,%s,%s);"
+        cursor.execute(query, (pid, price, stock, description, brand))
+        attr = cursor.fetchone()
+        cursor.close()
+    return True
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
@@ -92,7 +102,7 @@ def home():
             if log_in(username[1:], password, as_admin=True):
                 response.set_cookie('admin', username)
                 response.set_cookie('username', username)
-        elif log_in(username, password): 
+        elif log_in(username, password):
             response.set_cookie('username', username)
         return response
     return render_template("index.html", data='logged out', form=form)
@@ -120,14 +130,19 @@ def register():
         return render_template('register.html', form=form)
 
 
-@app.route("/admin", methods=['GET', 'POST'])
+@app.route("/adminpage", methods=['GET', 'POST'])
 def admin():
-    if not is_logged_in(check_admin=True):
-        return redirect(url_for('home'))
+    #if not is_logged_in(check_admin=True):
+    #    return redirect(url_for('home'))
     form = AdminAddProduct()
     if request.method == 'POST':
         if form.validate_on_submit():
-            newProduct = Product()
+            pid = form.productID.data
+            stock = form.stock.data
+            description = form.description.data
+            price = form.price.data
+            brand = form.brand.data
+            addProduct(pid, stock, description, price, brand)
 
     return render_template("adminpage.html", form=form)
 
